b4c0f2a4c502880a298f9c27b8552f9a93806899
Xinny <xingping@yobota.xyz>
2019-04-25 14:32:08 +0100

    added get_daily_pollution_stats to get_data

--- get_data.py
+++ get_data.py
@@ -5,56 +5,102 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from typing import List
+from titlecase import titlecase
 
 
 class GetData:
     """A class to get data from a specific local authority"""
 
     def __init__(self, authority_name=None, url='http://api.erg.kcl.ac.uk/AirQuality/'):
-        self.authority_name = authority_name.title()
+        self.authority_name = titlecase(authority_name)
         self.url = url
 
-    def get_local_authority_data(self, url_suffix):
+    def get_local_authority_data(self, url_suffix='Information/MonitoringLocalAuthority/GroupName=London/Json',
+                                 get_id=False):
         r = requests.get(self.url + url_suffix)
         data = r.json()['LocalAuthorities']['LocalAuthority']
         if self.authority_name is None:
             pass
         elif self.authority_name is not None:
             for item in data:
-                if item['@LocalAuthorityName'] == self.authority_name:
+                if item['@LocalAuthorityName'] == self.authority_name and get_id == False:
                     data = item
+                elif item['@LocalAuthorityName'] == self.authority_name and get_id == True:
+                    data = item['@LocalAuthorityCode']
         else:
             print('Please insert a valid name')
         return data
 
-    def get_pollution_data(self, local_authority_id, year):
-        url_suffix = '/Annual/MonitoringObjective/LocalAuthorityId={0}/Year={1}/Json'.format(str(local_authority_id),
-                                                                                             str(year))
-        r = requests.get(self.url + url_suffix)
-        json_data = json.loads(codecs.decode(r.content, 'utf-8-sig'))
+    def get_pollution_data(self, local_authority_id, years: List):
         pollution_df = pd.DataFrame({'species_code': [], 'species_desc': [],
                                      'objective_name': [], 'value': [],
                                      'site_name': [],
                                      'year': []})
-        try:
-            for item in json_data['SiteObjectives']['Site']:
-                for part in item['Objective']:
-                    pollution_df = pollution_df.append({'species_code': part['@SpeciesCode'],
-                                                        'species_desc': part['@SpeciesDescription'],
-                                                        'objective_name': part['@ObjectiveName'],
-                                                        'value': part['@Value'],
-                                                        'site_name': item['@SiteName'],
-                                                        'year': part['@Year']}, ignore_index=True)
-        except TypeError:
-            list_data = json_data['SiteObjectives']['Site']
-            for item in list_data['Objective']:
-                pollution_df = pollution_df.append({'species_code': item['@SpeciesCode'],
-                                                    'species_desc': item['@SpeciesDescription'],
-                                                    'objective_name': item['@ObjectiveName'],
-                                                    'value': item['@Value'],
-                                                    'site_name': list_data['@SiteName'],
-                                                    'year': item['@Year']}, ignore_index=True)
+        for year in years:
+            url_suffix = '/Annual/MonitoringObjective/LocalAuthorityId={0}/Year={1}/Json'.format(
+                str(local_authority_id),
+                str(year))
+            r = requests.get(self.url + url_suffix)
+            json_data = json.loads(codecs.decode(r.content, 'utf-8-sig'))
+
+            try:
+                for item in json_data['SiteObjectives']['Site']:
+                    for part in item['Objective']:
+                        pollution_df = pollution_df.append({'species_code': part['@SpeciesCode'],
+                                                            'species_desc': part['@SpeciesDescription'],
+                                                            'objective_name': part['@ObjectiveName'],
+                                                            'value': part['@Value'],
+                                                            'site_name': item['@SiteName'],
+                                                            'year': part['@Year']}, ignore_index=True)
+            except TypeError:
+                list_data = json_data['SiteObjectives']['Site']
+                for item in list_data['Objective']:
+                    pollution_df = pollution_df.append({'species_code': item['@SpeciesCode'],
+                                                        'species_desc': item['@SpeciesDescription'],
+                                                        'objective_name': item['@ObjectiveName'],
+                                                        'value': item['@Value'],
+                                                        'site_name': list_data['@SiteName'],
+                                                        'year': item['@Year']}, ignore_index=True)
         return pollution_df
+
+    def get_daily_pollution_stats(self, local_authority_ids: List):
+        daily_pollution_df = pd.DataFrame({'species_code': [], 'site_name': [], 'air_quality_index': [],
+                                           'air_quality_band': [], 'site_code': []})
+        for item in local_authority_ids:
+            url_suffix = '/Daily/MonitoringIndex/Latest/LocalAuthorityId={0}/Json'.format(str(item))
+            r = requests.get(self.url + url_suffix)
+            json_data = r.json()
+
+            try:
+                list_data = json_data['DailyAirQualityIndex']['LocalAuthority']['Site']
+                for item in list_data:
+                    if type(item['Species']) == list:
+                        for part in item['Species']:
+                            daily_pollution_df = daily_pollution_df.append({'species_code': part['@SpeciesCode'],
+                                                                            'site_code': item['@SiteCode'],
+                                                                            'air_quality_index': part['@AirQualityIndex'],
+                                                                            'air_quality_band': part['@AirQualityBand'],
+                                                                            'site_name': item['@SiteName']},
+                                                                           ignore_index=True)
+                    elif type(item['Species']) == dict:
+                        daily_pollution_df = daily_pollution_df.append({'species_code': item['Species']['@SpeciesCode'],
+                                                                        'site_code': item['@SiteCode'],
+                                                                        'air_quality_index': item['Species'][
+                                                                            '@AirQualityIndex'],
+                                                                        'air_quality_band': item['Species'][
+                                                                            '@AirQualityBand'],
+                                                                        'site_name': item['@SiteName']}, ignore_index=True)
+            except TypeError:
+                dict_data = json_data['DailyAirQualityIndex']['LocalAuthority']['Site']
+                for item in dict_data['Species']:
+                    daily_pollution_df = daily_pollution_df.append({'species_code': item['@SpeciesCode'],
+                                                                    'site_code': dict_data['@SiteCode'],
+                                                                    'air_quality_index': item['@AirQualityIndex'],
+                                                                    'air_quality_band': item['@AirQualityBand'],
+                                                                    'site_name': dict_data['@SiteName']}, ignore_index=True)
+
+
+        return daily_pollution_df
 
     @staticmethod
     def plot_pollution_by_species(species, data_list: List):
--- tests/test_get_data.py
+++ tests/test_get_data.py
@@ -2,7 +2,6 @@ import pytest
 from get_data import GetData
 
 
-def test_get_pollution_data():
-    assert GetData.get_pollution_data(2, 2018) == 'hhjfgsd'
-
-
+def get_local_authority_data():
+    hackney = GetData(authority_name='Hackney')
+    assert type(hackney) == str
